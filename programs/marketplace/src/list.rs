8918d82da126b6837cc021876848c75d6ea018f9
mestan998 <kunal170107028@alumni.iitg.ac.in>
2023-03-23 21:18:36 +0530

    First Commit

---
+++ programs/marketplace/src/list.rs
@@ -0,0 +1,134 @@
+use {
+    anchor_lang::{
+        prelude::*,
+        system_program,
+    },
+    anchor_spl::{
+        associated_token,
+        token,
+    },
+    program_error::ProgramError,
+};
+pub fn list(
+    ctx: Context<ListNFT>,
+    expected_amount: u64
+) -> Result<()> {
+
+    // msg!("Initiating transfer of {} lamports...", sale_lamports);
+    // msg!("Purchaser (sending lamports): {}", &ctx.accounts.buyer_authority.key());
+    // msg!("Seller (receiving lamports): {}", &ctx.accounts.owner_authority.key());
+    // system_program::transfer(
+    //     CpiContext::new(
+    //         ctx.accounts.system_program.to_account_info(),
+    //         system_program::Transfer {
+    //             from: ctx.accounts.buyer_authority.to_account_info(),
+    //             to: ctx.accounts.owner_authority.to_account_info(),
+    //         }
+    //     ),
+    //     sale_lamports
+    // )?;
+    let nft_mint =  &ctx.accounts.mint;
+    let seller_token_account = &ctx.accounts.seller_token_account;
+    let seller_wallet = &ctx.accounts.seller_wallet;
+    let escrow = &ctx.accounts.escrow;
+    let escrow_ata =  &ctx.accounts.token_account;
+    let token_program = &ctx.accounts.token_program;
+    let system_program = &ctx.accounts.system_program;
+    let ata_program = &ctx.accounts.associated_token_program;
+    let rent = &ctx.accounts.rent;
+    //*Assign values to the Escrow */
+    escrow.is_initialized = true;
+    escrow.seller_pubkey = seller_wallet.key();
+    escrow.token_account_pubkey = seller_token_account;
+    escrow.mint_key = nft_mint;
+    escrow.expected_amount = expected_amount;
+    let token_account_state = token::state::Account::unpack(
+        &**token_account.data.borrow()
+    ).unwrap();
+    // check if the token account have balance
+    if token_account_state.amount != (1 as u64){
+        msg!("invalid NFT data ** ..");
+        return Err(ProgramError::InvalidAccountData);
+    }
+
+    
+    msg!("Lamports transferred successfully.");
+
+    msg!("Creating buyer token account...");
+    msg!("Buyer Token Address: {}", &ctx.accounts.buyer_token_account.key());    
+    associated_token::create(
+        CpiContext::new(
+            ctx.accounts.associated_token_program.to_account_info(),
+            associated_token::Create {
+                payer: ctx.accounts.buyer_authority.to_account_info(),
+                associated_token: ctx.accounts.buyer_token_account.to_account_info(),
+                authority: ctx.accounts.buyer_authority.to_account_info(),
+                mint: ctx.accounts.mint.to_account_info(),
+                system_program: ctx.accounts.system_program.to_account_info(),
+                token_program: ctx.accounts.token_program.to_account_info(),
+                rent: ctx.accounts.rent.to_account_info(),
+            },
+        ),
+    )?;
+
+    msg!("Transferring NFT...");
+    msg!("Owner Token Address: {}", &ctx.accounts.owner_token_account.key());    
+    msg!("Buyer Token Address: {}", &ctx.accounts.buyer_token_account.key());    
+    token::transfer(
+        CpiContext::new(
+            ctx.accounts.token_program.to_account_info(),
+            token::Transfer {
+                from: ctx.accounts.owner_token_account.to_account_info(),
+                to: ctx.accounts.buyer_token_account.to_account_info(),
+                authority: ctx.accounts.owner_authority.to_account_info(),
+            }
+        ),
+        1
+    )?;
+    msg!("NFT transferred successfully.");
+    
+    msg!("Sale completed successfully!");
+
+    Ok(())
+}
+// fn find_pda(seeds, program_id) {
+//     for bump in 0..256 {
+//       let potential_pda = hash(seeds, bump, program_id);
+//       if is_pubkey(potential_pda) {
+//         continue;
+//       }
+//       return (potential_pda, bump);
+//     }
+//     panic!("Could not find pda after 256 tries.");
+// }
+#[account]
+pub struct Escrow {
+    pub is_initialized: bool,
+    pub seller_pubkey: Pubkey,
+    pub token_account_pubkey: Pubkey,
+    pub mint_key: Pubkey,
+    pub expected_amount: u64,
+}
+
+#[derive(Accounts)]
+pub struct ListNFT<'info> {
+    #[account(mut)]
+    pub mint: Account<'info, token::Mint>,
+    #[account(mut)]
+    pub seller_token_account: Account<'info, token::TokenAccount>,
+    #[account(mut)]
+    pub seller_wallet: Signer<'info>,
+    #[account(mut)]
+    pub escrow_token_account: UncheckedAccount<'info>,
+    #[account(mut,
+        init,
+        payer = user,
+        space = 8 + 32 + 32 + 32 + 8 + 1, seeds = [b"marketplace", seller_wallet.key().as_ref()],
+        bump
+    )]
+    pub escrow: AccountInfo<'info, Escrow>,
+    pub rent: Sysvar<'info, Rent>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, token::Token>,
+    pub associated_token_program: Program<'info, associated_token::AssociatedToken>,
+}
